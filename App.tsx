5feef46d43cd2cd4f77839de73c57fa968c67fd0
Gabriel Nogueira <bbynogueira@gmail.com>
2021-07-22 22:05:01 -0300

    * Setted up the theme

--- App.tsx
+++ App.tsx
@@ -2,13 +2,27 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { NavigationTabs } from "./app/src/infrastructure/navigation/navigation-tabs";
 import { Provider as PaperProvider } from "react-native-paper";
+import { theme } from "./app/src/infrastructure/theme";
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
-  return (
-    <PaperProvider>
-      <NavigationContainer>
-        <NavigationTabs />
-      </NavigationContainer>
-    </PaperProvider>
-  );
+  const [oswaldLoaded] = useOswald({ Oswald_400Regular });
+  const [latoLoaded] = useLato({ Lato_400Regular });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  } else {
+    return (
+      <PaperProvider theme={theme}>
+        <NavigationContainer>
+          <NavigationTabs />
+        </NavigationContainer>
+      </PaperProvider>
+    );
+  }
 }
---
+++ app/src/components/spacer/spacer.component.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled, { useTheme } from 'styled-components/native';
+
+const positionVariant = {
+  left: 'margin-left',
+  right: 'margin-right',
+  top: 'margin-top',
+  bottom: 'margin-bottom',
+};
+
+const sizeVariant = {
+  small: 1,
+  medium: 2,
+  large: 3,
+};
+
+const getVariant = (position, size, theme) => {
+  const property = positionVariant[position];
+  const sizeIndex = sizeVariant[size];
+  const value = theme.space[sizeIndex];
+  return `${property}: ${value}`;
+};
+
+const SpacerView = styled.View`
+  ${({ variant }) => variant};
+`;
+
+export const Spacer = ({ position, size, children }) => {
+  const theme = useTheme();
+  const variant = getVariant(position, size, theme);
+  return <SpacerView variant={variant}>{children}</SpacerView>;
+};
+
+Spacer.defaultProps = {
+  position: 'top',
+  size: 'small',
+};
