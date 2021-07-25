152b2a4eec7e2aadacb36552ed679229bd66c8bc
Gabriel Nogueira <bbynogueira@gmail.com>
2021-07-25 18:04:33 -0300

    * Created TextInput Component

--- app/src/features/wab/styles/passenger-list.styles.tsx
+++ app/src/features/wab/styles/passenger-list.styles.tsx
@@ -1,8 +1,5 @@
 import styled from 'styled-components/native';
-import { TextInput } from 'react-native-paper';
 
 export const View = styled.View`
     flex: 1;
 `;
-
-export const TextInput = styled.TextInput``;
